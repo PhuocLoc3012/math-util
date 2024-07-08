e5f546654931d00a5faf052ea716be39802d4b12
PhuocLoc <135364306+PhuocLoc3012@users.noreply.github.com>
2024-07-08 15:42:08 +0700

    refactor the main code and green 4ever always

--- src/com/locbp/mathutil/core/MathUtil.java
+++ src/com/locbp/mathutil/core/MathUtil.java
@@ -18,17 +18,40 @@ public class MathUtil {
     //0! = 1
     //giai thừa tăng nhanh về giá trị
     //tính n giai thừa trong khoảng 0 - 20
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0-20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+//    
+    //Học đệ quy trong 30s
+    //Hiện tượng gọi lại chính mình với 1 quy mô khác
+    //n! = 1.2.3.4.5.6.....n
+    //6! = 6 x 5!
+    //5! = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1! //điểm dừng
+    //1! = 1;
+    //quy ước 1! = 0! = 1;
+    //chốt hạ: n! = n * (n - 1)!
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) {
+        if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid argument. N must be between 0-20");
-        }
         if (n == 0 || n == 1) {
             return 1;
         }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+
+        return n * getFactorial(n - 1); //công thức đệ quy
+        
     }
+
 }
